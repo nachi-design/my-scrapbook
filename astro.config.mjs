c2e019351f58158e6b98909507d19d4259893160
nachi-design <mszmszmszloq@icloud.com>
2025-03-16 11:13:21 +0900

    GitHubPagesでのデプロイ対応

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://nachi-design.github.io',
+    base: 'my-scrapbook_new',
+    // base: process.env.NODE_ENV === 'production' ? '/my-scrapbook_new/' : '/',
+});
