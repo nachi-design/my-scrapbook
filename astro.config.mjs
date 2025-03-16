250f43d39ec9e863e93dba086e6e6d5732dd8a36
nachi-design <mszmszmszloq@icloud.com>
2025-03-16 15:11:35 +0900

    リポジトリ名変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://nachi-design.github.io',
-    base: 'my-scrapbook_new',
-    // base: process.env.NODE_ENV === 'production' ? '/my-scrapbook_new/' : '/',
+    base: 'my-scrapbook',
+    // base: process.env.NODE_ENV === 'production' ? '/my-scrapbook/' : '/',
 });
